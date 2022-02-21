c5a5c070db9fddf7006778d823396a9468629241
Dzmitry Paplauski <dmitrypoplavskij@gmail.com>
2022-02-21 20:30:36 +0100

    invincible zombie

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -13,6 +13,8 @@ final class GameScene: SKScene {
     let playableRect: CGRect
     let zombieRotateRadiansPerSec: CGFloat = .pi * 4
     let zombieMovePointsPerSec: CGFloat = 480.0
+
+    private var isZombieInvincible: Bool = false
     
     override init(size: CGSize) {
         let playableHeight = size.width / UIScreen.main.landscapeAspectRation
@@ -129,8 +131,12 @@ final class GameScene: SKScene {
     }
     
     func checkZombieCollisions() {
-        for enemy in hitEnemies {
-            zombieHit(characterNode: enemy)
+        if isZombieInvincible {
+            return
+        }
+
+        if !hitEnemies.isEmpty {
+            makeZombieInvincible(for: 5.0, blinkTimes: 8)
             run(.playSoundFileNamed("hitCatLady.wav", waitForCompletion: false))
         }
         for cat in hitCats {
@@ -140,6 +146,9 @@ final class GameScene: SKScene {
     }
 
     func zombieHit(characterNode: SKSpriteNode) {
+        if isZombieInvincible {
+            return
+        }
         characterNode.removeFromParent()
     }
     
@@ -236,6 +245,22 @@ final class GameScene: SKScene {
         )
     }
 
+    private func makeZombieInvincible(for duration: TimeInterval, blinkTimes: Int) {
+        isZombieInvincible = true
+
+        let blink = SKAction.customAction(withDuration: duration) { zombie, elapsedTime in
+            let slice = duration / Double(blinkTimes)
+            let remainder = Double(elapsedTime).truncatingRemainder(dividingBy: slice)
+
+            zombie.isHidden = remainder > slice / 2
+        }
+        let setVincible = SKAction.run {
+            self.isZombieInvincible = false
+        }
+        let blinkTillVincible = SKAction.sequence([blink, setVincible])
+        zombie.run(blinkTillVincible)
+    }
+
     private func zombieCollisionsWithNodes(named: String, insets: UIEdgeInsets = .zero) -> [SKSpriteNode] {
         var nodes: [SKSpriteNode] = []
         enumerateChildNodes(withName: named) { [weak self] node, _ in
@@ -265,12 +290,6 @@ final class GameScene: SKScene {
     private var velocity = CGPoint.zero
     private let intervalCounter = UpdateIntervalCounter()
     private let zombie = Character.zombie.node
-}
-
-private extension CGFloat {
-    mutating func changeSign() {
-        self = -self
-    }
 }
 
 private extension UIScreen {
