0c12aae547afc21f156905009d7bef89f07d17a2
Dzmitry Paplauski <dmitrypoplavskij@gmail.com>
2022-02-21 20:30:16 +0100

    refactoring

---
+++ ZombieConga/Extensions/CGFloat+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  CGFloat+Extensions.swift
+//  ZombieConga
+//
+//  Created by Dimasno1 on 21.02.22.
+//  Copyright © 2022 Dimasno1. All rights reserved.
+//
+
+import Foundation
+
+private extension CGFloat {
+    mutating func changeSign() {
+        self *= -1
+    }
+}
